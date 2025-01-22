a7bad5cbaebcc5a6e32a5b0e63cb26fabd582aa1
jakubkanna <hello.jakubkanna@gmail.com>
2025-01-22 13:22:16 +0100

    404 add redirecting from old pages

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,16 +1,31 @@
-import { Link } from "react-router-dom";
-import Layout from "../components/layout/Layout.";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { Col, Container } from "react-bootstrap";
+import { Helmet } from "react-helmet";
+import { useEffect } from "react";
+import Layout from "../components/layout/Layout.";
 
 export default function NotFoundPage() {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // Check if the location contains '/index.php' and redirect if needed
+    if (location.pathname.includes("/index.php")) {
+      navigate("/");
+    }
+  }, [location, navigate]);
+
   return (
     <Container>
-      <Layout title="404 Not Found">
+      <Helmet>
+        <title>404 Not Found</title>
+      </Helmet>
+      <Layout>
         <Col className="position-fixed top-50 start-50 translate-middle text-center font-monospace">
           <p>Page not found :(</p>
           <Link to={"/"}>Home</Link>
         </Col>
-      </Layout>{" "}
+      </Layout>
     </Container>
   );
 }
